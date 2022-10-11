8149c4a32ac7708de6093d5cdb6237b833b001c6
mohammedkhalid079 <mohammedkhalid079@gmail.com>
2022-10-11 15:54:30 +0530

    categories and category components and home components as well as done withs scss files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
+import "./components/category-item/category-item.styles.scss";
+import Home from "./routes/home-components/home.component";
+
 const App = () => {
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Ball</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+    <section>
+      <Home />
+    </section>
   );
 };
 
