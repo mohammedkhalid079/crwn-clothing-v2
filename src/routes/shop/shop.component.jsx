2e8772d670b4e06fe7642191ba865b12a3f4f22c
mohammedkhalid079 <mohammedkhalid079@gmail.com>
2022-11-03 14:02:32 +0530

    created categories-preview and implemented nested routes in shop.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,11 @@
 //react automatically entire json file stored inside Shop_Data.
 // import SHOP_DATA from "../../shop-data.json";
 
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   // const { searchFields } = useContext(CategoriesContext);
   // const [filteredProducts, setFilteredProducts] = useState(products);
 
@@ -22,14 +20,9 @@ const Shop = () => {
   // }, [products, searchFieldsLowerCase]);
 
   return (
-    <div className=" shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
