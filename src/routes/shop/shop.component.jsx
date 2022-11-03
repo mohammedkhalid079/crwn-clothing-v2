43331d0bc5f659a88d56a4ba1bd86b662b4da207
mohammedkhalid079 <mohammedkhalid079@gmail.com>
2022-11-03 13:14:26 +0530

    created category-preview as well updated shop comp.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,9 @@
 //react automatically entire json file stored inside Shop_Data.
 // import SHOP_DATA from "../../shop-data.json";
 
-import { Fragment, useContext, useState } from "react";
+import { useContext } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
@@ -22,19 +22,14 @@ const Shop = () => {
   // }, [products, searchFieldsLowerCase]);
 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {console.log(title)}
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className=" shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
